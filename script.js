8caf6501741307199fa4cdc3a06d1638df8a4d65
RuanAssis <ruan.assis@eaportal.org>
2024-03-25 23:02:18 -0300

    segundo commit do JS

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let conta = 0
+let pessoas = 0
+let porcentagem = 0
 
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
@@ -15,12 +17,21 @@ function receberQuantidadePessoas(evento){
     const divErro = document.querySelector(".pessoas .input-box")
 
     if(evento.target.value === "0"){
-        paragrafoErro.computedStyleMap.display = "block"
+        paragrafoErro.style.display = "block"
         divErro.setAttribute("id", "erro-div")
 
     } else {
-        paragrafoErro.computedStyleMap.display = "none"
+        paragrafoErro.style.display = "none"
         divErro.setAttribute("id", "")
         pessoas = Number(evento.target.value)
     }
+}
+
+const botoesGorjeta = document.querySelector(".gorjeta input[type='button']")
+botoesGorjeta.forEach(botao => {
+    botao.addEventListener("click")
+})
+
+function receberPorcentagem(evento){
+    porcentagem  = Number(evento.target.value)
 }
